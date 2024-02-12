1976f20d8e6973e8eceae61ee39f82888275df7f
yyyytawa <yyyytawa@qq.com>
2024-02-12 12:01:38 +0800

    updata

--- src/.vuepress/navbar.ts
+++ src/.vuepress/navbar.ts
@@ -2,20 +2,11 @@ import { navbar } from "vuepress-theme-hope";
 
 export default navbar([
   "/",
-  {
-   {
-   text: "文档",
-   icon: "book",
-   prefix: "/guide/",
-  },
-  {
-   text: "Blog",
-   prefix: "/blog/",
-  },
+  "/blog/",
+  "/guide/",
   {
     text: "Github主页",
     icon: "/assets/image/github-dark.svg",
     link: "https://github.com/yyyytawa",
-  },
  },
 ]);
