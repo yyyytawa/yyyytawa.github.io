ff206a7801cfdb5665cb1c22e336c8e49422451a
yyyytawa <yyyytawa@qq.com>
2024-06-01 23:56:56 +0800

    祝大家儿童节快乐! Feat:更改侧边栏,更新14周内容

--- src/.vuepress/sidebar.ts
+++ src/.vuepress/sidebar.ts
@@ -1,18 +1,6 @@
 import { sidebar } from "vuepress-theme-hope";
 
 export default sidebar({
-  "/": [
-    "",
-    {
-      text: "文档",
-      icon: "book",
-      prefix: "guide/",
-      children: "structure",
-    },
-    {
-      text: "Blog",
-      prefix: "blog/",
-      children: "structure",
-    },
-  ],
+  "/blog/": "structure",
+  "/guide/": "structure",
 });
