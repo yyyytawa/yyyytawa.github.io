7edfbfb2b4ec442693d269dc8376d68b108b4bb4
yyyytawa <116234899+yyyytawa@users.noreply.github.com>
2024-10-05 13:40:34 +0800

    Update: PWA

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -10,6 +10,6 @@ export default defineUserConfig({
 
   theme,
   // 和 PWA 一起启用
-  // shouldPrefetch: false,
+   shouldPrefetch: false,
   
 });
--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -145,60 +145,21 @@ export default hopeTheme({
     },
 
     // 如果你需要 PWA。安装 vuepress-plugin-pwa2 并取消下方注释
-    // pwa: {
-    //   favicon: "/favicon.ico",
-    //   cacheHTML: true,
-    //   cachePic: true,
-    //   appendBase: true,
-    //   apple: {
-    //     icon: "/assets/icon/apple-icon-152.png",
-    //     statusBarColor: "black",
-    //   },
-    //   msTile: {
-    //     image: "/assets/icon/ms-icon-144.png",
-    //     color: "#ffffff",
-    //   },
-    //   manifest: {
-    //     icons: [
-    //       {
-    //         src: "/assets/icon/chrome-mask-512.png",
-    //         sizes: "512x512",
-    //         purpose: "maskable",
-    //         type: "image/png",
-    //       },
-    //       {
-    //         src: "/assets/icon/chrome-mask-192.png",
-    //         sizes: "192x192",
-    //         purpose: "maskable",
-    //         type: "image/png",
-    //       },
-    //       {
-    //         src: "/assets/icon/chrome-512.png",
-    //         sizes: "512x512",
-    //         type: "image/png",
-    //       },
-    //       {
-    //         src: "/assets/icon/chrome-192.png",
-    //         sizes: "192x192",
-    //         type: "image/png",
-    //       },
-    //     ],
-    //     shortcuts: [
-    //       {
-    //         name: "Demo",
-    //         short_name: "Demo",
-    //         url: "/demo/",
-    //         icons: [
-    //           {
-    //             src: "/assets/icon/guide-maskable.png",
-    //             sizes: "192x192",
-    //             purpose: "maskable",
-    //             type: "image/png",
-    //           },
-    //         ],
-    //       },
-    //     ],
-    //   },
-    // },
+     pwa: {
+       favicon: "/favicon.ico",
+       cacheHTML: true,
+       appendBase: true,
+       update: "disable",
+       apple: {
+         icon: "/assets/icon/apple-icon-152.png",
+         statusBarColor: "black",
+       },
+       msTile: {
+         image: "/assets/icon/ms-icon-144.png",
+         color: "#ffffff",
+       },
+       manifest: {
+       },
+     },
   },
 });
