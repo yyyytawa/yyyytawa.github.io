a19234e40da78c0159be57951b8251668055ce36
yyyytawa <116234899+yyyytawa@users.noreply.github.com>
2024-11-24 14:46:43 +0800

    Update: See commit

--- src/.vuepress/sidebar.ts
+++ src/.vuepress/sidebar.ts
@@ -7,4 +7,6 @@ export default sidebar({
   "/others/notice/": "structure",
   // 自传
   "/articles/books/autobiography/": "structure",
+  // 诗和歌
+  "/articles/poems_and_songs/": "structure",
 });
---
+++ src/articles/poems_and_songs/random/README.md
@@ -0,0 +1,11 @@
+---
+title: 随手写写
+order: 1
+index: false
+dir:
+    link: true
+---
+
+<div class="catalog-display-container">
+  <Catalog base='/articles/poems_and_songs/random/' />
+</div>
