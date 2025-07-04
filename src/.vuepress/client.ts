1d977547e77c349abc3c668156e52b83fd01882d
yyyytawa <116234899+yyyytawa@users.noreply.github.com>
2025-07-05 06:18:17 +0800

    Update: 更改为局部注册 减少js体积

--- src/.vuepress/client.ts
+++
@@ -1,8 +0,0 @@
-import { defineClientConfig } from "vuepress/client";
-import Sticker from './components/Sticker.vue'
-
-export default defineClientConfig({
-  enhance: ({ app, router, siteData }) => {
-    app.component('Sticker', Sticker)
-    }
-})
